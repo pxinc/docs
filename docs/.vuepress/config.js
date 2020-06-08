5ba903f9a9fd687bf18c534fa4553eef2825e41f
web-pxc <412876701@qq.com>
2020-06-08 21:56:28 +0800

    feat: 新增域名部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,17 +3,16 @@ const PLUGINS_CONF = require('./config/plugins.js')
 const HEAD_CONF = require('./config/head.js')
 
 module.exports = {
-    base: '/blog/',
     title: 'Pxinc Blog',
     description: "Pxinc Blog By VuePress",
     head: HEAD_CONF,
-    lastUpdated: '更新时间',
     extraWatchFiles: [
         '../**/*.md',
         './config.js'
     ],
     themeConfig: {
         docsDir: 'docs',
+        lastUpdated: '更新时间',
         nav: NAV_CONF,
     },
     plugins: PLUGINS_CONF
--- docs/README.md
+++ docs/README.md
@@ -4,9 +4,6 @@ title: Home
 heroImage: /app.png
 heroText: P.X.C
 tagline: 文档, 博客
-actionText: 开始升级 →
+actionText: 开始升级
 actionLink: /base/
 ---
-
-## 哈哈
-阿萨德拉斯的
